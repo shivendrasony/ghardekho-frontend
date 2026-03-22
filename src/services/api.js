5369186788c14125b0f5f836d310fea0f458963a
Shivendra Sony <shivendrasony22@gmail.com>
2026-03-22 18:08:57 +0530

    Connect frontend to Railway backend

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api',
+})
+
+api.interceptors.request.use(config => {
+  const user = JSON.parse(localStorage.getItem('ghardekho_user') || '{}')
+  if (user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+})
+
+export default api
